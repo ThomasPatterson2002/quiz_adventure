0c69bd3cb325780e26f9e44fb6b0cb76df8d98ba
thomas patterson <tpatterson18@VenerableBede.schools.sunderland.gov.uk>
2016-11-17 09:56:40 +0000

    done

--- adventure.py
+++ adventure.py
@@ -1,8 +1,131 @@
 # An adventure game
-
+from time import sleep
 def room1():
 
-    ... make a game
+    print("You wake up in the atic of an old house")
+    sleep(2)
+    print("You see a door")
+    sleep(2)
+    print("You also see a window")
+    sleep(2)
+    print("You also also see a stairway down")
+    sleep(2)
+    print("Which one do you choose")
+    sleep(2)
+    print("A. go though door")
+    sleep(2)
+    print("B. go through the window (not the best choice)")
+    sleep(2)
+    print("C. go down the stairs")
+    answer = input("?")
+
+    if answer.lower() == "b":
+        print("You idiot, you fall to your death")
+        sleep(2)
+        print("Your character: no actualy im fine")
+        sleep(2)
+        print("You are then ripped apart by the guard dogs")
+        sleep(2)
+        print("Now you dead")
+        sleep(1)
+        print("GAME OVER")
+        sleep(3)
+        quit()
+    elif answer.lower() == "c":
+        print ("You tumble to your death like granny")
+        sleep(2)
+        print("R.I.P")
+        sleep(3)
+        quit()
+    elif answer.lower() == "a":
+        print("the door is locked")
+    room2()
+def room2():
+    print("what do you do")
+    sleep(2)
+    print("A. look for key(wast of time)")
+    sleep(2)
+    print("B. stare at door for eternity(even bigger wast of time)")
+    answer = input("?")
+
+    if answer.lower() == "b":
+        print("why did you choose that!")
+        sleep(2)
+        print("After 60 years of staring at the door(the best years of your life)you die")
+        sleep(3)
+        quit()
+    elif answer.lower() == "a":
+        print("you could not find the key(told you it was a wast of time)")
+        sleep(2)
+        print("Your character: i found a flash light!")
+        sleep(2)
+        print("Oh")
+        sleep(2)
+    room3()
+def room3():
+    print("what do you want to do with the flash light?")
+    sleep(2)
+    print("A. shine it at the stairs so you can see were your going")
+    sleep(2)
+    print("B. throw it out the window(why is this an option)")
+    answer = input("?")
+
+    if answer.lower() == "a":
+        print("you discover that it was never a stairway but instead a shear drop")
+        sleep(2)
+        print("and die for some reason")
+        sleep(3)
+        quit()
+    elif answer.lower() == "b":
+        print("you idiot, the only thing left to do is jump out after it")
+        sleep(2)
+        print("but wait")
+        sleep(10)
+        print("that was a long wait")
+        sleep(2)
+        print("the flash light has distracted the guard dog")
+        sleep(2)
+        print("if you didnt choose option B for question 1 you wont know what im talking about")
+        sleep(2)
+        print("you manage to escape while the guard dog is distracted")
+        sleep(2)
+        print("Oh...wait its coming back")
+
+    room4()
+def room4():
+    print("your about to die again what do you do")
+    sleep(1)
+    print("A. run for your life")
+    sleep(2)
+    print("B. insert stupid option here")
+    answer = input("?")
+
+    if answer.lower() == "a":
+        print("you hurt you legs from the fall")
+        sleep(2)
+        print("once again you are inevitably killed in a horific way")
+        sleep(2)
+        print("so close")
+        sleep(2)
+        print("im sorry but...")
+        sleep(2)
+        print("here it comes...")
+        sleep(2)
+        print("any minute now...")
+        sleep(2)
+        print("and...")
+        sleep(2)
+
+        print("GAME OVER")
+        sleep(3)
+        quit()
+    elif answer.lower() == "b":
+        print("you fly away or somthing im to lazy to think of an ending")
+        sleep(2)
+        print("well done your not dead")
+        sleep(3)
+        quit()
+        
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,8 +1,66 @@
 # Our quiz!
-
+score = 0
 def quiz():
+    
+    q1()
+def q1():
+    global score
+    
+    print("Gandalf or Dumbledore????????")
+    print("A. Gandalf")
+    print("B. Dimpledoor")
+    answer = input("CHOOSE ONE!!!!!!!:")
 
-    ... make a quiz!
+    if answer.lower() == "a":
+        print("Run you fools!")
+        print("Score is")
+        score = score + 1
+        print (score)
+    q2()
+def q2():
+    
+    global score
+    
+    print("NEXT QUESTION!!!!!")
+    print("Golom or Smegle")
+    print("A. Golom")
+    print("B. Smegle")
+    answer = input("CHOOSE ONE!!!!!!!:")
+
+    if answer.lower() == "a" or "b":
+        print ("IT DOESNT MATTER THEY'RE BOTH THE SAME!")
+        score("Score is")
+        score = score +1
+        print(score)
+    q3()
+def q3():
+    global score
+
+    print("LAST QUESTION!")
+    print("THIS IS THE MOST IMPORTANT QUESTION")
+    print("was this the best quiz ever made")
+    print("A. YES!!!!!")
+    print("B. less enthusiastic yes")
+    answer = input("CHOOSE ONE!!!!!!!:")
+
+    if answer.lower() == "a":
+        print ("THATS WHAT I THOUGHT")
+        print ("Score is")
+        score = score +9999998
+        print(score)
+    elif answer.lower() == "b":
+        print ("I did'nt think anyone would pick that one")
+        print ("Score is")
+        score = score - score
+        print (score)
+        
+        
+    
+          
+    
+    
+
+    
 
 
 
